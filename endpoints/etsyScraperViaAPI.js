d5b76963a9c24174e0c4bd83ba22a24e19120193
david586 <d.harding1020@gmail.com>
2024-01-15 19:04:09 +0700

    Mon

--- endpoints/etsyScraperViaAPI.js
+++ endpoints/etsyScraperViaAPI.js
@@ -5,7 +5,6 @@ const fsPromises = require('fs').promises;
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const FormData = require('form-data');
 const router = express.Router();
-const axios = require('axios');
 const url = require('url');
 const path = require('path');
 const fetch = require("node-fetch");
@@ -36,7 +35,8 @@ async function refreshTokens(refresh_token) {
 
 async function getApiCall(_url, _clientID, _accessToken, retries = 3) {
     try {
-        const response = await axios.get(`${_url}`, {
+        const response = await fetch(_url, {
+            method: 'GET',
             headers: {
                 'x-api-key': _clientID,
                 Authorization: `Bearer ${_accessToken}`,
@@ -44,14 +44,20 @@ async function getApiCall(_url, _clientID, _accessToken, retries = 3) {
                 'Content-Type': 'application/json'
             },
         });
-        return response.data;
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
     } catch (error) {
-        console.error(`Failed API call for ${url}: ${error}`);
+        console.error(`Failed API call for ${_url}: ${error}`);
         if (retries > 0) {
-            console.log(`Retrying API call for ${url}, (${retries} attempts left)...`);
-            return getApiCall(url, _clientID, _accessToken, retries - 1);
+            console.log(`Retrying API call for ${_url}, (${retries} attempts left)...`);
+            return getApiCall(_url, _clientID, _accessToken, retries - 1);
         } else {
-            console.error(`No more retries left for ${url}`);
+            console.error(`No more retries left for ${_url}`);
             return null;
         }
     }
@@ -79,26 +85,28 @@ router.post('/', async (req, res) => {
 
     const {shop_id, first_name, scrapeButtonType} = req.body;
     let {refresh_token, access_token} = req.body;
-    var user_submitted_lines = req.body.textInput.split('\n');
+    const user_submitted_lines = req.body.textInput.split('\n');
+    const {limit_results} = req.body;
 
-    if (scrapeButtonType === 'scrapebuttonsspecificsection') {
-        console.log(`Scraping specific section/s`);
-    } else {
-        console.log(`Scraping entire shop/s`);
-    }
+    scrapeButtonType === 'scrapebuttonspecificsection' ? console.log(`Scraping specific section/s`) : null;
+    scrapeButtonType === 'scrapebuttonphrase' ? console.log(`Scraping by phrase`) : null;
+    scrapeButtonType === 'scrapebuttonentireshop' ? console.log(`Scraping entire shop/s`) : null;
+
+    limit_results !== '' ? console.log(`Limiting results to ${limit_results} per line`) : console.log(`Not limiting results`);
 
     let allListingData = [];
 
     for (let row of user_submitted_lines) {
 
-        let startingLength = allListingData.length;
+        let currentRowListingData = [];
 
         let splitRow = row.split(',');
         let user_provided_shop_id = splitRow[0];
         let user_provided_section_id;
+        let user_provided_phrase;
 
         // Get the section ID
-        if (scrapeButtonType === 'scrapebuttonsspecificsection') {
+        if (scrapeButtonType === 'scrapebuttonspecificsection') {
             let user_provided_section_url = splitRow[1];
             if (user_provided_section_url === undefined) {
                 // Handle the error here
@@ -112,13 +120,38 @@ router.post('/', async (req, res) => {
             }
         }
 
-        // Just an exit if the user provided a section id but clicked the wrong button
+        // Get the phrase
+        if (scrapeButtonType === 'scrapebuttonphrase') {
+            user_provided_phrase = splitRow[1];
+            if (user_provided_phrase === undefined) {
+                // Handle the error here
+                console.error('No phrase provided');
+                console.log(row);
+                res.status(400).send('No phrase provided');
+                return;
+            } else {
+                // Check if the provided phrase is a valid URL
+                try {
+                    new URL(user_provided_phrase);
+                    console.error('URL provided when user has clicked scrape by phrase button.');
+                    console.log(row);
+                    res.status(400).send('URL provided when user has clicked scrape by phrase button.');
+                    return;
+                } catch (_) {
+                    // Not a valid URL, continue with the process
+                }
+                user_provided_phrase = user_provided_phrase.trim().toLowerCase();
+                console.log(`Searching for phrase: ${user_provided_phrase}`);
+            }
+        }
+
+        // Just an exit if the user provided a section id or phrase but clicked the wrong button
         if (scrapeButtonType === 'scrapebuttonentireshop') {
             let user_provided_section_url = splitRow[1];
             if (user_provided_section_url !== undefined) {
-                console.error('Section URL provided when scraping entire shop. Did you mean to select "Scrape Specific Section"?');
+                console.error('Section URL or Phrase provided when user has clicked scrape entire shop button.');
                 console.log(row);
-                res.status(400).send('Section URL provided when scraping entire shop. Did you mean to select "Scrape Specific Section"?');
+                res.status(400).send('Section URL or Phrase provided when user has clicked scrape entire shop button.');
                 return;
             }
         }
@@ -131,25 +164,44 @@ router.post('/', async (req, res) => {
                 getListingsResponse = await getApiCall(all_listings_api_url + `&offset=${page_offset}`, clientID, access_token);
 
                 // If we're not limiting it by section then push all listing ids
-                if (scrapeButtonType !== 'scrapebuttonsspecificsection') {
-                    const listingData = getListingsResponse.results.map(listing => (
+                if (scrapeButtonType === 'scrapebuttonentireshop') {
+                    const listingData = getListingsResponse.results
+                    .map(listing => (
                         {
                             listingID: listing.listing_id,
                             title: listing.title
                         }
                     ));
-                    allListingData.push(...listingData);
-                } else {
-                    // Otherwise only push listing ids that are in the correct section
+                    currentRowListingData.push(...listingData);
+                } else if (scrapeButtonType === 'scrapebuttonspecificsection') {
+                    // Only push listing ids that are in the correct section
                     const listingData = getListingsResponse.results
-                        .filter(listing => listing.shop_section_id == user_provided_section_id)
+                    .filter(listing => listing.shop_section_id == user_provided_section_id)
                         .map(listing => (
                             {
                                 listingID: listing.listing_id,
                                 title: listing.title
                             }
                         ));
-                    allListingData.push(...listingData);
+                        currentRowListingData.push(...listingData);
+                } else if (scrapeButtonType === 'scrapebuttonphrase') {
+                    // Only push listing ids that have the correct phrase in the title
+                    const listingData = getListingsResponse.results
+                    // res.send(`<pre>${JSON.stringify(listingData, null, 2)}</pre>`);
+                    // return;
+                    .filter(listing => listing.title.toLowerCase().includes(user_provided_phrase))
+                        .map(listing => (
+                            {
+                                listingID: listing.listing_id,
+                                title: listing.title
+                            }
+                        ));
+                        currentRowListingData.push(...listingData);
+                }
+
+                //If we're limiting the results, then break out of the loop if we've reached the limit
+                if (limit_results !== '' && currentRowListingData.length >= limit_results) {
+                    break;
                 }
 
                 if (getListingsResponse.results.length < 100) {
@@ -162,24 +214,26 @@ router.post('/', async (req, res) => {
             console.error(`Failed to fetch all listings at page offset ${page_offset}: ${error}`);
         }
 
-        console.log(`Found ${allListingData.length - startingLength} matching listings for: ${row}`);
-        // res.send(`<pre>${JSON.stringify(allListingData, null, 2)}</pre>`);
-        // return;
-
-        // Now for each listing, get the image url
-        let getImagesResponse;
-        for (let listing of allListingData) {
-            let indiv_listing_api_url = `https://openapi.etsy.com/v3/application/listings/${listing.listingID}/images`;
-            getImagesResponse = await getApiCall(indiv_listing_api_url, clientID, access_token);
-            listing.imageURL = getImagesResponse.results[0].url_fullxfull;
-            // rate limiting
-            await delay(500);
+        if (limit_results !== '') {
+            currentRowListingData = currentRowListingData.slice(0, limit_results);
         }
-
-        // res.send(`<pre>${JSON.stringify(allListingData, null, 2)}</pre>`);
-        // return;
+        console.log(`Found ${currentRowListingData.length} matching listings for: ${row}`);
+        allListingData.push(...currentRowListingData);
 
     }
+
+    // Now for each listing, get the image url
+    let getImagesResponse;
+    for (let listing of allListingData) {
+        let indiv_listing_api_url = `https://openapi.etsy.com/v3/application/listings/${listing.listingID}/images`;
+        getImagesResponse = await getApiCall(indiv_listing_api_url, clientID, access_token);
+        listing.imageURL = getImagesResponse.results[0].url_fullxfull;
+        // rate limiting
+        await delay(500);
+    }
+
+    // res.send(`<pre>${JSON.stringify(allListingData, null, 2)}</pre>`);
+    // return;
 
     ////// So now allListingData is an array of objects with listingID, title, and imageURL //////
     console.log('Starting image download process')
@@ -206,9 +260,11 @@ router.post('/', async (req, res) => {
     // Function to download image
     async function downloadImage(url, filePath, retries = 3) {
         try {
-            const response = await axios.get(url, { responseType: 'arraybuffer' });
-            if (response.data.byteLength > 0) {
-                await fs.promises.writeFile(filePath, response.data);
+            const response = await fetch(url);
+            const buffer = await response.buffer();
+    
+            if (buffer.byteLength > 0) {
+                await fsPromises.writeFile(filePath, buffer);
             } else {
                 throw new Error(`Received empty data. URL: ${url}, Filename: ${filePath}`);
             }
